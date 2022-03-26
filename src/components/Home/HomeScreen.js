fd35836c7c6aad710cfef2fd81dfb8d60fe2e861
Kishore <55543092+kishore13-dotmaker@users.noreply.github.com>
2022-03-27 01:18:26 +0530

    Enter

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {useState} from "react";
 import ActionButton from 'react-native-action-button';
 import {
   SafeAreaView,
@@ -22,9 +22,64 @@ import shops from "../consts/shops";
 
 const { width } = Dimensions.get("screen");
 
-const Home = ({ navigation }) => {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      dataSource: [],
+      checkin: "",
+      checkout: "",
+    };
+  }
+  async componentDidMount() {
+    
+    const accessToken = await SecureStore.getItemAsync("accessToken");
+    var details = {
+      accessToken: accessToken,
+    };
+    var formBody = [];
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
 
-  return (
+    }
+    formBody = formBody.join("&");
+    const postResponse = fetch("http://172.19.17.164:3000/findBookings", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: formBody,
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.foundBookings,
+        });
+      });
+  }
+  renderItem = ({item}) => (
+    <TouchableOpacity onPress={() => {}}>
+    <View style={BookingHistoryStyles.contentontainerStyle}>
+      <Text>{item.checkIn}</Text>
+      <Text>{item.checkOut}</Text>
+      </View>
+    </TouchableOpacity>
+    
+  );
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={BookingHistoryStyles.container}>
+          <ActivityIndicator size="large" animating />
+        </View>
+      );
+    } else {
+      return (
     <SafeAreaView style={HomeStyles.safeArea}>
       <StatusBar
         translucent={false}
@@ -66,8 +121,21 @@ const Home = ({ navigation }) => {
             </Pressable>
           )}
         />
+        <View style={BookingHistoryStyles.container}>
+          <FlatList
+        snapToInterval={width - 20}
+        contentontainerStyle={BookingHistoryStyles.contentontainerStyle}
+        showsHorizontalScrollIndicator={false}
+        vertical={true}
+        data={this.state.dataSource}
+        renderItem={this.renderItem}                                                                                                                                                                                                     
+        keyExtractor={(item,index) => index}      
+        />
+        </View>
     </SafeAreaView>
   );
+}
+
+  }
 };
 
-export default Home;

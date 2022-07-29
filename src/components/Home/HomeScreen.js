e4b4f92f11bb1aa15b6f13fddcc37a69241ccaf0
kishore13-dotmaker <kishorem168@gmail.com>
2022-07-29 20:21:31 +0530

    format

--- src/components/Home/HomeScreen.js
+++ src/components/Home/HomeScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import React,{useState} from "react";
-import ActionButton from 'react-native-action-button';
+import React, { useState } from "react";
+import ActionButton from "react-native-action-button";
 import {
   SafeAreaView,
   Text,
@@ -18,23 +18,22 @@ import SearchBar from "../SearchBar/SerachBar";
 import ListOptions from "../ListOptions/ListOptions";
 import Categories from "../Categories/Categories";
 import Card from "../ShopCards/card";
-import * as SecureStore from 'expo-secure-store';
-import {ip} from "./IpAddress"
+import * as SecureStore from "expo-secure-store";
+import { ip } from "./IpAddress";
 const { width } = Dimensions.get("screen");
 
 const Home = ({ navigation }) => {
+  const [fromDate, setFromDate] = useState();
+  const [toDate, setToDate] = useState();
+  const [booking, setBooking] = useState();
 
-  const [fromDate,setFromDate]=useState();
-  const [toDate,setToDate]=useState();
-  const [booking,setBooking]=useState();
-
-  const handleSubmit =async () => { 
-    var accessToken = await SecureStore.getItemAsync('accessToken');
+  const handleSubmit = async () => {
+    var accessToken = await SecureStore.getItemAsync("accessToken");
     var details = {
       todate: toDate,
       fromdate: fromDate,
       accessToken: accessToken,
-    }
+    };
     var formBody = [];
     for (var property in details) {
       var encodedKey = encodeURIComponent(property);
@@ -42,26 +41,26 @@ const Home = ({ navigation }) => {
       formBody.push(encodedKey + "=" + encodedValue);
     }
     formBody = formBody.join("&");
-    fetch(ip+'/findBookings', {
-      method: 'POST',
+    fetch(ip + "/findBookings", {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
       },
-      body: formBody
+      body: formBody,
     })
-		.then((response) => response.json())
-    .then( async (response) =>{
-      try {
-        setBooking(response.checkIn)
-      } catch (e) {
-        console.log(e)
-      }
-}).catch((error)=>{
-      console.error(error);
-    });
-
-  }
+      .then((response) => response.json())
+      .then(async (response) => {
+        try {
+          setBooking(response.checkIn);
+        } catch (e) {
+          console.log(e);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   const hotelProfile = async () => {
     var accessToken = await SecureStore.getItemAsync("accessToken");
@@ -75,7 +74,7 @@ const Home = ({ navigation }) => {
       formBody.push(encodedKey + "=" + encodedValue);
     }
     formBody = formBody.join("&");
-    fetch(ip+"/findHotel", {
+    fetch(ip + "/findHotel", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -86,66 +85,70 @@ const Home = ({ navigation }) => {
       .then((response) => response.json())
       .then(async (responseJson) => {
         await SecureStore.setItemAsync("username", responseJson.hotel.username);
-        await SecureStore.setItemAsync("hotelName", responseJson.hotel.hotelName);
+        await SecureStore.setItemAsync(
+          "hotelName",
+          responseJson.hotel.hotelName
+        );
         navigation.navigate("Profile");
       });
   };
   return (
     <SafeAreaView style={HomeStyles.safeArea}>
-       <StatusBar
+      <StatusBar
         translucent={false}
         backgroundColor={Colors.white}
         barstyle="dark-content"
-      /> 
-      
+      />
+
       <View style={HomeStyles.header}>
         <View>
           <Text style={{ color: Colors.greyHome }}></Text>
           <Text
             style={{ color: Colors.black, fontSize: 20, fontWeight: "bold" }}
-          >Booking Details
+          >
+            Booking Details
           </Text>
         </View>
         <Pressable onPress={() => hotelProfile()}>
-        <Image
-          source={require("../../assets/images/app_icon/profile.jpg")}
-          style={HomeStyles.profileImage}
-        />
+          <Image
+            source={require("../../assets/images/app_icon/profile.jpg")}
+            style={HomeStyles.profileImage}
+          />
         </Pressable>
       </View>
-      
-        {/* <SearchBar />
+
+      {/* <SearchBar />
         <Categories /> */}
-        <TextInput
-                style={HomeStyles.input}
-                labelValue={fromDate}
-                onChangeText={(fromDate) => setFromDate(fromDate)}
-                placeholder="From Date"
-        />
-                <TextInput
-                style={HomeStyles.input}
-                labelValue={toDate}
-                onChangeText={(toDate) => setToDate(toDate)}
-                placeholder="To Date"
-        />
+      <TextInput
+        style={HomeStyles.input}
+        labelValue={fromDate}
+        onChangeText={(fromDate) => setFromDate(fromDate)}
+        placeholder="From Date"
+      />
+      <TextInput
+        style={HomeStyles.input}
+        labelValue={toDate}
+        onChangeText={(toDate) => setToDate(toDate)}
+        placeholder="To Date"
+      />
       <Pressable
-                style={[HomeStyles.button, HomeStyles.buttonClose]}
-                onPress={() => handleSubmit()}
-              >
-                <Text style={HomeStyles.textStyle}>Confirm Date</Text>
-              </Pressable>
-              
-        <FlatList
-          snapToInterval={width - 20}
-          contentontainerStyle={HomeStyles.contentontainerStyle}
-          showsHorizontalScrollIndicator={false}
-          vertical={true}
-          data={booking}
-          keyExtractor={( item , index) => {return item._id}}
-          renderItem={({ item }) => (
-              <Card item={item} />
-          )}
-        />
+        style={[HomeStyles.button, HomeStyles.buttonClose]}
+        onPress={() => handleSubmit()}
+      >
+        <Text style={HomeStyles.textStyle}>Confirm Date</Text>
+      </Pressable>
+
+      <FlatList
+        snapToInterval={width - 20}
+        contentontainerStyle={HomeStyles.contentontainerStyle}
+        showsHorizontalScrollIndicator={false}
+        vertical={true}
+        data={booking}
+        keyExtractor={(item, index) => {
+          return item._id;
+        }}
+        renderItem={({ item }) => <Card item={item} />}
+      />
     </SafeAreaView>
   );
 };
--- src/components/Login/LoginScreen.js
+++ src/components/Login/LoginScreen.js
@@ -1,28 +1,25 @@
-import React, {useEffect, useContext, useState} from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
-import FormInput from '../Input/FormInput';
-import FormButton from '../Buttons/FormButton';
-import DismissKeyboard from '../../utils/DismissKeyboard';
-import LoginStyles from './LoginStyles';
+import React, { useEffect, useContext, useState } from "react";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import FormInput from "../Input/FormInput";
+import FormButton from "../Buttons/FormButton";
+import DismissKeyboard from "../../utils/DismissKeyboard";
+import LoginStyles from "./LoginStyles";
 // import { AuthContext } from '../../navigation/AuthProviders';
-import SocialButtons  from "../Buttons/SocialButtons";
-import * as SecureStore from 'expo-secure-store';
-import {ip} from '../Home/IpAddress'
+import SocialButtons from "../Buttons/SocialButtons";
+import * as SecureStore from "expo-secure-store";
+import { ip } from "../Home/IpAddress";
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const [isloggedin,setLogged] = useState();
-
-
- 
+  const [isloggedin, setLogged] = useState();
 
   // const {login} = useContext(AuthContext);
-  const handleSubmit = () => { 
+  const handleSubmit = () => {
     var details = {
       username: email,
       password: password,
-    }
+    };
     var formBody = [];
     for (var property in details) {
       var encodedKey = encodeURIComponent(property);
@@ -30,90 +27,87 @@ const LoginScreen = ({navigation}) => {
       formBody.push(encodedKey + "=" + encodedValue);
     }
     formBody = formBody.join("&");
-    fetch(ip+'/loginHotel', {
-      method: 'POST',
+    fetch(ip + "/loginHotel", {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
       },
-      body: formBody
+      body: formBody,
     })
-		.then((response) => response.json())
-    .then( async (responseJson) =>{
-      try {
-        await SecureStore.setItemAsync('accessToken',responseJson.accessToken)
-        navigation.replace("Home")
-      } catch (e) {
-        console.log(e)
-      }
-})
-    
-    .catch((error)=>{
-      console.error(error);
-    });
-  }
-    
+      .then((response) => response.json())
+      .then(async (responseJson) => {
+        try {
+          await SecureStore.setItemAsync(
+            "accessToken",
+            responseJson.accessToken
+          );
+          navigation.replace("Home");
+        } catch (e) {
+          console.log(e);
+        }
+      })
+
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <DismissKeyboard>
-    <View style={LoginStyles.container}>
-      <Image
-        source={require('../../assets/images/app_icon/Phoenixlogo.png')}
-        style={LoginStyles.logo}
-        resizeMode='contain'
-      />
-      <Text style={LoginStyles.text}>Hotel-1 Partner</Text>
+      <View style={LoginStyles.container}>
+        <Image
+          source={require("../../assets/images/app_icon/Phoenixlogo.png")}
+          style={LoginStyles.logo}
+          resizeMode="contain"
+        />
+        <Text style={LoginStyles.text}>Hotel-1 Partner</Text>
 
-      <FormInput
-        labelValue={email}
-        onChangeText={(userEmail) => setEmail(userEmail)}
-        placeholderText="Email"
-        iconType="user"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
+        <FormInput
+          labelValue={email}
+          onChangeText={(userEmail) => setEmail(userEmail)}
+          placeholderText="Email"
+          iconType="user"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
 
-      <FormInput
-        labelValue={password}
-        onChangeText={(userPassword) => setPassword(userPassword)}
-        placeholderText="Password"
-        iconType="lock"
-        secureTextEntry={true}
-      />
+        <FormInput
+          labelValue={password}
+          onChangeText={(userPassword) => setPassword(userPassword)}
+          placeholderText="Password"
+          iconType="lock"
+          secureTextEntry={true}
+        />
 
-      <FormButton
-        buttonTitle="Sign In"
-        onPress={() => handleSubmit()}
-        
-      />
+        <FormButton buttonTitle="Sign In" onPress={() => handleSubmit()} />
 
-      <TouchableOpacity style={LoginStyles.forgotButton} onPress={() => navigation.navigate('ForgotPassword')}>
-        <Text style={LoginStyles.navButtonText}>Forgot Password?</Text>
-      </TouchableOpacity>
-      
-      
-      {/* <SocialButtons
+        <TouchableOpacity
+          style={LoginStyles.forgotButton}
+          onPress={() => navigation.navigate("ForgotPassword")}
+        >
+          <Text style={LoginStyles.navButtonText}>Forgot Password?</Text>
+        </TouchableOpacity>
+
+        {/* <SocialButtons
         googleButtonViewStyle= {LoginStyles.googleSignInBackground} 
         googleLogoStyle= {LoginStyles.googleSignInBackground}
         googleTextStyle= {LoginStyles.googleSignInText}
       
       /> */}
-      
 
-      <TouchableOpacity
-        style={LoginStyles.forgotButton}
-        onPress={() => navigation.navigate('SignUp')}
+        <TouchableOpacity
+          style={LoginStyles.forgotButton}
+          onPress={() => navigation.navigate("SignUp")}
         >
-        <Text style={LoginStyles.navButtonText}>
-          Don't have an acount? Create here
-          
-        </Text>
-      </TouchableOpacity>
-    </View>
+          <Text style={LoginStyles.navButtonText}>
+            Don't have an acount? Create here
+          </Text>
+        </TouchableOpacity>
+      </View>
     </DismissKeyboard>
   );
 };
 
 export default LoginScreen;
-
